8e47c8877ca4b0caa5dc45a1f535563ad49c4793
PulledPork0 <67171394+PulledPork0@users.noreply.github.com>
2020-06-30 01:22:09 -0700

    fix up solve_linear

--- algocomp/solve_linear.py
+++ algocomp/solve_linear.py
@@ -3,63 +3,67 @@ from .tracked_number import coerce_int as _int
 from .gcd import xgcd
 
 
-# NOTE: not much thought was put into solve_linear
-#       should probably follow up on it
-
 def solve_linear(a,b,c):
-    """return (x,y) such that a*x + b*y = c"""
-
-    # It would be nice to have a, in some sense, "minimal" solution.
-    # unsure if there is a nicer / more uniform was of handling this
+    """return (x,y) such that a*x + b*y = c, and |x| minimized"""
 
     # Start by checking some special cases first
     if a==0:
         if b==0:
             assert c==0
             return (0,0)
-        assert (_int(c) % _int(b))==0   # use _int to bypass cost for assert
+        assert (_int(c) % _int(b)) == 0   # use _int to bypass cost for assert
         x = 0
         y = c//b
         return (x,y)
 
     if b==0:
-        assert (_int(c) % _int(a))==0   # use _int to bypass cost for assert
+        assert (_int(c) % _int(a)) == 0   # use _int to bypass cost for assert
         x = c//a
         y = 0
         return (x,y)
 
-    if abs(a) > abs(b):
-        if (c%a)==0:
-            return (c//a, 0)
-        if (c%b)==0:
-            return (0, c//b)
-    else:
-        if (c%b)==0:
-            return (0, c//b)
-        if (c%a)==0:
-            return (c//a, 0)
-
-    g,x,y = xgcd(a,b)
+    # solve: a u + b v = g = gcd(a,b)
+    g,u,v = xgcd(a,b)
     assert (_int(c) % _int(g))==0     # use _int to bypass cost for assert
 
-    # Is there a more direct way to get the "minimal" solution?
-    # this creates a large answer, and then reduces it
-    c = (c//g)
-    x *= c
-    y *= c
+    """
+    given
+        a u + b v = g = gcd(a,b)
+    then
+        a x + b y = c
+    has the solution (with a freedom in 'n')
+        x = u (c/g) + n b
+        y = v (c/g) - n a
 
-    # There is still some freedom
-    # x -> x - bn, y -> y + an
-    # use this to reduce the answer
-    if abs(b)<abs(a):
-        n = x//b
-        x -= n*b
-        y += n*a
+    using the freedom in n, we can get the minimal x
+        -b/2 < x <= b/2
+    according to:
+    x = (u c / g) % b
+    if x > b/2:
+        x = x - b
+
+    This can be improved with a minor trick used by Cohen and others,
+    (x > b/2) is the same as checking (2x > b), thus (x > b - x)
+
+    So the following is equivalent only using negation and the difference
+    which is a value we want anyway if changing the value of x.
+
+    diff = b - x
+    if x > diff:
+        x = -diff
+    """
+
+    if g == 1:
+        x = (u*c) % b  # note: b=0 already handled in special case
     else:
-        n = y//a
-        x += n*b
-        y -= n*a
+        x = (u*(c//g)) % b  # note: b=0 already handled in special case
+
+    diff = b - x
+    if x > diff:
+        x = -diff
+
+    # now with the minimal x, just solve for y
+    y = (c - a*x)//b
 
     return (x,y)
-
 
